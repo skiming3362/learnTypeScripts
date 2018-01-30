954b9d43f0221ad90a7f37c23f42d9df1a593120
chenming <chenming@uxmc.cn>
2018-01-30 18:13:43 +0800

    update

--- officialDocs.ts
+++ officialDocs.ts
@@ -1,2 +1,16 @@
 let list: number[] = [1, 2, 3];
 const a = 1;
+let list2: Array<number> = [1, 2, 3];
+
+let x: [string, number];
+
+x = ['', 2];
+x[3] = true; //访问越界元素， 使用联合类型替代
+
+enum Color {Red = 1, Green = 2, Blue = 4}
+
+let c: Color = Color.Green;
+
+let colorName: string = Color[2];
+
+console.log(colorName);
