82ad999e5645cf51b77a7b684ed6b9ef7011d9c5
chenming <chenming@uxmc.cn>
2018-01-31 17:33:39 +0800

    interfaces

--- officialDocs.ts
+++ officialDocs.ts
@@ -60,3 +60,119 @@ let o = {
 let { a, b = 'foo' } = o;
 
 let plus = [0, ...rest];
+
+// 接口
+
+// 对象类型
+interface LabelledValue {
+    label: string;
+}
+
+function printLabel(labelledObj: LabelledValue): void {
+    console.log(labelledObj.label);
+}
+
+let myObj = { label: '123', size: 10 };
+printLabel(myObj);
+
+// 可选属性
+
+interface SquareConfig {
+    color?: string;
+    width?: number;
+    [propName: string]: any;
+}
+
+function createSquare(config: SquareConfig): { color: string, area: number } {
+    let newSquare = { color: 'white', area: 100 };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+
+let mySquare = createSquare({ color: 'black', size: 10 });
+
+// 只读属性
+
+interface Point {
+    readonly x: number;
+    readonly y: number;
+}
+
+let p1: Point = { x: 10, y: 20 };
+
+let ro: ReadonlyArray<number> = [1, 2, 3, 4];
+
+list = ro; // list = ro as number[]
+
+// 额外属性检查 82行, 跳过检查 =》 将对象赋值给另一个变量， 一般不应该跳过
+
+// 函数类型
+
+interface SearchFunc {
+    (source: string, subString: string): boolean;
+}
+
+let mySearch: SearchFunc;
+
+mySearch = (src, sub) => {
+    let result = src.search(sub);
+    return result > -1;
+};
+
+// 可索引类型
+
+interface StringArray {
+    [index: number]: string;
+}
+
+let myArray: StringArray;
+myArray = ['bob', 'fred'];
+
+let myStr = myArray[0];
+
+class Animal {
+    name: string;
+}
+
+class Dog extends Animal {
+    breed: string;
+}
+
+interface NotOkay {
+    [x: number]: Animal;
+    [x: string]: Dog;
+}
+
+interface NumberDictionary {
+    [index: string]: number;
+    length: number;    // 可以，length是number类型
+    name: string;     // 错误，`name`的类型与索引类型返回值的类型不匹配
+}
+
+interface ReadonlyStringArray {
+    readonly [index: number]: string;
+}
+let myArray2: ReadonlyStringArray = ['Alice', 'Bob'];
+myArray2[2] = 'Mallory'; // error!
+
+// 类类型
+
+interface ClockInterface {
+    currentTime: Date;
+    setTime(d: Date);
+}
+
+class Clock implements ClockInterface {
+    currentTime: Date;
+    constructor(h: number, m: number) {
+        console.log(123);
+    }
+    setTime(d: Date) {
+        this.currentTime = d;
+    }
+}
